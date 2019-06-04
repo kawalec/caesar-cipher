f898d8a4437f35573985d112e27d85ccaa509e59
kawalec <kawalec@gmail.com>
2019-06-04 10:31:11 +0200

    added shift

--- app.js
+++ app.js
@@ -10,22 +10,32 @@ for (let i = 0; i < 2; i++) {
   let btn = document.createElement("button");
   btn.className = "btn";
   if (i === 0) {
-    btn.innerHTML = "Plain";
-    btn.id = "Encrypt";
+    btn.innerHTML = "Encrypt";
+    btn.id = "Plain";
   } else {
-    btn.innerHTML = "ASCII";
-    btn.id = "Decrypt";
+    btn.innerHTML = "Decrypt";
+    btn.id = "ASCII";
   }
   elem.appendChild(btn);
 }
+let label = document.createElement("lebel");
+label.innerHTML = "Shift";
+label.style.color = "#fff";
+let shift = document.createElement("input");
+shift.style.marginLeft = "10px";
+shift.style.width = "50px";
+shift.type = "number";
+label.appendChild(shift);
+elem.appendChild(label);
+
 document.body.appendChild(elem);
 
 document.querySelectorAll(".btn").forEach(el => {
   el.addEventListener("click", function(el) {
-    if (this.id == "Encrypt") {
+    if (this.id == "Plain") {
       let plain = document.querySelector("#Plain").value;
       document.querySelector("#ASCII").value = encrypt(plain);
-    } else if (this.id == "Decrypt") {
+    } else if (this.id == "ASCII") {
       let encoded = document.querySelector("#ASCII").value;
       document.querySelector("#Plain").value = decrypt(encoded);
     }
@@ -33,15 +43,37 @@ document.querySelectorAll(".btn").forEach(el => {
 });
 
 let encrypt = text => {
+  let shift = isNaN(parseInt(document.querySelector("input").value))
+    ? 0
+    : parseInt(document.querySelector("input").value);
   return text
     .split("")
-    .map(char => char.charCodeAt(0))
+    .map(char => {
+      if (char.charCodeAt(0) + shift > 127) {
+        return char.charCodeAt(0) + shift - 127;
+      } else if (char.charCodeAt(0) + shift < 0) {
+        return char.charCodeAt(0) + shift + 127;
+      } else {
+        return char.charCodeAt(0) + shift;
+      }
+    })
     .join("-");
 };
 
 let decrypt = text => {
+  let shift = isNaN(parseInt(document.querySelector("input").value))
+    ? 0
+    : parseInt(document.querySelector("input").value);
   return text
     .split("-")
-    .map(code => String.fromCharCode(code))
+    .map(code => {
+      if (code - shift < 0) {
+        return String.fromCharCode(code - shift + 127);
+      } else if (code - shift > 127) {
+        return String.fromCharCode(code - shift - 127);
+      } else {
+        return String.fromCharCode(code - shift);
+      }
+    })
     .join("");
 };
